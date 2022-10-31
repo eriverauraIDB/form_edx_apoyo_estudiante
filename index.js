d040df9e02a8d844c897fcf9fea5ca35fc43dd51
Elizabeth <elizabethriver@gmail.com>
2022-10-31 17:39:57 -0400

    input values

---
+++ index.js
@@ -0,0 +1,15 @@
+const form = document.getElementById("subdmitForm");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+//   inputs values
+  const inputName = document.getElementById("inputName").value;
+  const inputEmail = document.getElementById("inputEmail").value;
+  const inputDescription = document.getElementById("inputDescription").value;
+  const inputIssue = document.getElementById("inputIssue").value;
+  const inputCourse = document.getElementById("inputCourse").value;
+  const browserSelected =  document.querySelector('input[name=browserSelected]:checked').value;
+  const inputSystemOperativeSelect =  document.querySelector('input[name=systemOperativeSelect]:checked').value;
+  console.log(inputName,inputEmail, inputDescription, inputIssue, inputCourse, browserSelected,inputSystemOperativeSelect )
+});
